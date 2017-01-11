52885682c27b82044906ebdba354d57ba2732867
Florent TONNEAU <florent.tonneau@ensimag.grenoble-inp.fr>
2017-01-11 20:37:28 +0100

    FIX issue with service starting script

--- deployment/service.sh
+++ deployment/service.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          skeleton
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Should-Start:      $portmap
+# Should-Stop:       $portmap
+# X-Start-Before:    nis
+# X-Stop-After:      nis
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# X-Interactive:     true
+# Short-Description: Example initscript
+# Description:       This file should be used to construct scripts to be
+#                    placed in /etc/init.d.
+### END INIT INFO
 
 #Read config file
 while read line
 do
       var=$line
-done < "service.conf"
+done < "/etc/init.d/service.conf"
 
 DAEMON="/apps/$var.py"
 DAEMONUSER="root"
@@ -14,37 +29,16 @@ for i in $(echo $var | tr "/" "\n")
 do
   var=$i
 done
-daemon_NAME="$var.py"
+daemon_NAME="sombrero"
 
 PATH="/sbin:/bin:/usr/sbin:/usr/bin"
 
-test -x $DAEMON || exit 0
-
-. /lib/lsb/init-functions
-
-d_start () {
-        log_daemon_msg "Starting system $daemon_NAME Daemon"
-        start-stop-daemon --background --name $daemon_NAME --start --quiet --chuid $DAEMONUSER --exec $DAEMON -- $daemon_OPT
-        log_end_msg $?
-}
-
-d_stop () {
-        log_daemon_msg "Stopping system $daemon_NAME Daemon"
-        start-stop-daemon --name $daemon_NAME --stop --retry 5 --quiet --name $daemon_NAME
-        log_end_msg $?
-}
-
 case "$1" in
   start)
-        d_start
+        start-stop-daemon --background --name $daemon_NAME --start --quiet --chuid $DAEMONUSER --exec $DAEMON --
         ;;
-  restart|reload|force-reload)
-        echo "Error: argument '$1' not supported" >&2
-        exit 3
-        ;;
-  stop|status)
-        # No-op
-        d_stop
+  stop)
+        start-stop-daemon --name $daemon_NAME --stop --retry 5 --quiet
         ;;
   *)
         echo "Usage: $0 start|stop" >&2
